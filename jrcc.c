fc800fa2a79f4a54672d7c0a553bf08cd9448e2b
hamadatakaki <i16hamadat@gmail.com>
2019-08-20 01:06:18 +0900

    Add first parser

--- jrcc.c
+++ jrcc.c
@@ -20,6 +20,23 @@ struct Token {
   char *str;
 };
 
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
 Token *token;
 char *user_input;
 
@@ -107,6 +124,57 @@ Token *tokenize(char *p) {
 
   new_token(TK_EOF, cur, p);
   return head.next;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  while(1) {
+    if(consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if(consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else 
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = term();
+
+  while(1) {
+    if(consume('*'))
+      node = new_node(ND_MUL, node, term());
+    else if(consume('/'))
+      node = new_node(ND_DIV, node, term());
+    else
+      return node;
+  }
+}
+
+Node *term() {
+  if(consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_node_num(expect_number());
 }
 
 int main(int argc, char **argv) {
